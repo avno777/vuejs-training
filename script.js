008765129845e47f8824a7d2b0d87f1ff79eaec6
Nguyen Vu <nguyenanhvu2172000@gmail.com>
2023-08-31 01:46:36 +0700

    xu ly bat dong bo voi axios

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const fetchUsers = () => {
-  axios
-    .get("http://5d6e3cc8777f67001403658b.mockapi.io/list_user")
-    .then((response) => {
-      console.log(`GET list users`, response);
-      const users = response.data;
-      console.log("list users", users);
-    })
-    .catch((error) => console.error(error));
+const fetchUsers = async () => {
+  try {
+    alert("Bắt đầu");
+
+    const response = await axios.get(
+      "http://5d6e3cc8777f67001403658b.mockapi.io/list_user"
+    );
+    alert("Đã lấy dữ liệu");
+    console.log(response.data);
+
+    alert("Kết thúc gọi API");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 fetchUsers();
